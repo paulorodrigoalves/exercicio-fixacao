074beee035750beba11029592d145ed4c0732713
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2021-11-01 22:37:12 -0300

    Criação das Classes

---
+++ untitled/src/entities/Order.java
@@ -0,0 +1,70 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+
+    private Date moment;
+    private OrderStatus status;
+    private Client client;
+
+    List<OrderItem> list = new ArrayList<>();
+
+    public Order (){
+    }
+
+    public Order(Date moment, OrderStatus status, Client client) {
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public List getList(){
+        return list;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void addItem(OrderItem item){
+        list.add(item);
+    }
+
+    public void removeItem(OrderItem item){
+        list.remove(item);
+    }
+
+    public Double total(){
+        double total = 0.0;
+        for (OrderItem oi : list) {
+            total += oi.subTotal();
+        }
+        return total;
+    }
+
+
+}
